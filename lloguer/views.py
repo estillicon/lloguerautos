be763b403f2031ac1c3164dbff072935b63c9a67
estillicon <paugracia86@yahoo.com>
2025-03-28 16:44:36 +0100

    fita 5

--- lloguer/views.py
+++ lloguer/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Automobil
+
+def lista_automoviles(request):
+    # Obtener todos los automóviles de la base de datos
+    autos = Automobil.objects.all()
+    return render(request, 'lloguer/autos_lista.html', {'autos': autos})
+
